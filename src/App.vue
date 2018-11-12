b4ac589f8ed0ef5ab8db147f0fa386e6986ff48a
milktegi <milkteegi@gmail.com>
2018-11-12 11:20:32 +0900

    created vue pj

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+   <h1>{{title}}</h1>
+   <Navbar/>
+  </div>
+</template>
+
+<script>
+
+import Navbar from './Navbar'
+
+
+export default {
+  name: 'app',
+  components: {
+    Navbar: Navbar
+  }, 
+  data () {
+    return {
+      title: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+
+h1, h2 {
+  color:rgb(88, 88, 204);
+  font-weight: normal;
+
+}
+
+
+</style>
---
+++ src/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+	
+<nav>
+	<ul>
+		<li><a href=""></a>home</li>
+		<li><a href=""></a>about</li>
+		<li><a href=""></a>contact</li>
+	</ul>
+</nav>
+
+</template>
+
+<script>
+
+export default {
+	
+	name: 'Navbar',
+	data() {
+		return {
+			// returns an object
+		}
+	}
+}
+</script>
+
+<style>
+
+nav{
+	text-align: center;
+
+}
+nav ul{
+	padding: 0;
+}
+nav li{
+	display: inline-block;
+	list-style-type: none;
+	margin: 0;
+}
+
+</style>
