7a24fcd02c4fb7e4550981f0ad6e4e7a587b2672
milktegi <milkteegi@gmail.com>
2018-11-12 17:10:18 +0900

    done some practice with axios and filter

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,24 @@
   <div id="app">
    <h1>{{title}}</h1>
    <Navbar/>
-   <AllFriends :friends="friends" @delete="deleteFriend"/>
-   <OnlineFriends :friends="friends"/>
+    <Blogs/>
   </div>
 </template>
 
 <script>
 
 import Navbar from './Navbar'
-import AllFriends from './AllFriends'
-import OnlineFriends from './OnlineFriends'
-
+import Blogs from './Blogs'
 
 export default {
   name: 'app',
   components: {
     Navbar,
-    AllFriends,
-    OnlineFriends
+    Blogs,
   }, 
   data () {
     return {
-      title: 'Welcome to Your Vue.js App',
-      	friends: [
-				{ name: 'mario1', online: true },
-				{ name: 'mario2', online: false },
-				{ name: 'mario3', online: true },
-				{ name: 'mario4', online: false },
-			]
-
-    }
-  },
-  methods: {
-    deleteFriend(payload) {
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name;
-      })
+      title: 'my first vue app, wooo :) '
     }
   }
 }
---
+++ src/Blogs.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="blogs">
+		<h2>Blogs</h2>
+		<div v-for="post in posts" :key="post.id">
+		<h5>{{post.title}}</h5>	
+		<p>{{post.body | snippet }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+	name: 'Blogs',
+	data() {
+		return {
+			posts: []
+		}
+	},
+	methods: {
+
+	},
+	created() {
+		axios.get('https://jsonplaceholder.typicode.com/posts')
+		.then(response => {
+			this.posts = response.data
+		}).catch(err => {
+			console.log(err);
+		})
+	}
+}
+</script>
